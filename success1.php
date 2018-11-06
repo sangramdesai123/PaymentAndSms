ad36944ca2f4094697ceaa783d859b3dfcf33709
sangramdesai123 <sangram.desai@somaiya.edu>
2018-11-06 23:56:46 +0530

    added failure and sucess.php files

---
+++ success1.php
@@ -0,0 +1,19 @@
+<?php
+$status=$_POST["status"];
+$firstname=$_POST["firstname"];
+$amount=$_POST["amount"];
+$txnid=$_POST["txnid"];
+$posted_hash=$_POST["hash"];
+$key=$_POST["key"];
+$productinfo=$_POST["productinfo"];
+$email=$_POST["email"];
+$salt="";
+
+// Salt should be same Post Request 
+
+          echo "<h3>Thank You. Your order status is ". $status .".</h3>";
+          echo "<h4>Your Transaction ID for this transaction is ".$txnid.".</h4>";
+          echo "<h4>We have received a payment of Rs. " . $amount . ". Your order will soon be shipped.</h4>";
+          echo '<button class="btn btn=success"><a href="shop.php">Shop More</button>';
+		   
+?>	
